bb3336371fcdfc6feb811ad09313d23e7499b1d3
Osman Haider <osmanhaider159@gmail.com>
2024-09-19 00:06:43 +0600

    woocommerce filter hook applied

---
+++ functions/functions.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Ensure the file is not accessed directly.
+ */
+if (!defined('ABSPATH')) {
+    exit;
+}
+error_log(print_r('test', true));
+
+
+// Add text overlay on WooCommerce product image using woocommerce_product_get_image filter
+function add_text_overlay_to_product_image_html($image, $product, $size, $attr, $placeholder) {
+    // Custom overlay text
+    $overlay_text = 'Special Offer';
+
+    // Wrap the original image with a div and add overlay text
+    $custom_html = '<div class="custom-image-overlay">';
+    $custom_html .= $image; // Original image HTML
+    $custom_html .= '<span class="overlay-text">' . esc_html($overlay_text) . '</span>';
+    $custom_html .= '</div>';
+
+    return $custom_html;
+}
+// add_action( 'woocommerce_before_shop_loop_item_title', 'wish_me_add_icon_to_product_image' );
+add_filter('woocommerce_product_get_image', 'wish_me_add_icon_to_product_image', 10, 2);
+//	add_filter( 'woocommerce_cart_item_thumbnail', 'wish_me_add_icon_to_product_image', 10, 2 );
+// add_filter('woocommerce_single_product_image_html', 'wish_me_add_icon_to_product_image', 10, 2);
+// add_filter('woocommerce_single_product_image_thumbnail_html', 'wish_me_add_icon_to_product_image', 10, 2);
+// add_filter('woocommerce_single_product_image_thumbnail_html', 'wish_me_add_icon_to_product_image', 10, 2);
+
+
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,6 +6,10 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit8ca1004d92518d0dd9664a19c4b4e376
 {
+    public static $files = array (
+        'ac548b80908778fbd23763cdf0403c52' => __DIR__ . '/../..' . '/functions/functions.php',
+    );
+
     public static $prefixLengthsPsr4 = array (
         'S' => 
         array (
@@ -35,16 +39,11 @@ class ComposerStaticInit8ca1004d92518d0dd9664a19c4b4e376
         ),
     );
 
-    public static $classMap = array (
-        'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
-    );
-
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit8ca1004d92518d0dd9664a19c4b4e376::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit8ca1004d92518d0dd9664a19c4b4e376::$prefixDirsPsr4;
-            $loader->classMap = ComposerStaticInit8ca1004d92518d0dd9664a19c4b4e376::$classMap;
 
         }, null, ClassLoader::class);
     }
